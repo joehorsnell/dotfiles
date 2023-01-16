f2ac9713f46e025abeb0c39d2894655b068a7535
Joe Horsnell <github@joehorsnell.com>
2023-01-16 12:04:48 +0000

    Refactoring init.sh
    Getting rid of pointless `_if_required` suffix (functions should be idempotent by default).

--- init.sh
+++ init.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-install_homebrew_if_required() {
+install_homebrew() {
   if command -v brew >/dev/null 2>&1; then
     echo "Homebrew already installed 👍"
   else
     echo "Installing Homebrew 🍺"
+    # Install stanza from https://brew.sh
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
 }
 
 install_required_packages() {
-  # TODO - move this to a Brewfile
   declare -a packages=("curl" "fzf" "git" "stow")
   for package in "${packages[@]}"
   do
@@ -24,6 +24,15 @@ install_required_packages() {
   done
 }
 
+install_fzf_config() {
+  # Note: we know that the required config is already in zsh, so use --no-update-rc
+  [ ! -f ~/.fzf.zsh ] && $(brew --prefix)/opt/fzf/install --no-update-rc --key-bindings --completion
+}
+
+clone_dotfiles() {
+  [ ! -d ~/.dotfiles ] && git clone https://github.com/joehorsnell/dotfiles.git ~/.dotfiles
+}
+
 stow_packages() {
   declare -a packages=("asdf" "mackup" "psql" "ripgrep" "zsh")
   for package in "${packages[@]}"
@@ -33,18 +42,13 @@ stow_packages() {
   done
 }
 
-clone_dotfiles_if_required() {
-  [ ! -d ~/.dotfiles ] && git clone https://github.com/joehorsnell/dotfiles.git ~/.dotfiles
-}
-
-install_homebrew_if_required
-install_required_packages
-clone_dotfiles_if_required
-
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# fzf config
-# Note: we know that the required config is already in zsh, so use --no-update-rc
-[ ! -f ~/.fzf.zsh ] && $(brew --prefix)/opt/fzf/install --no-update-rc --key-bindings --completion
+install_homebrew
+install_required_packages
+install_fzf_config
+clone_dotfiles
+
+# (cd ~/.dotfiles && stow_packages)
 
 exit 0
